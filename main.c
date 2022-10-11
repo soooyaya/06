9c50cd436fe5b791b2c867f0d180413febc071e6
soooyaya <hsoo0531@naver.com>
2022-10-11 17:42:32 +0900

    05

--- main.c
+++ main.c
@@ -3,15 +3,39 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
  
- int square(int a)
- {
- 	return(a*a);
- }
+int factorial(int n)
+{
+	int res=1;
+	int i;
+	
+	for(i=1;i<=n;i++)
+		res=res*i;
+		
+		return(res);
+}
+
+int com(int n, int r){
+	int a, b, c;
+	int result;
+	
+	a = factorial(n);
+	b = factorial(n-r);
+	c = factorial(r);
+	
+	result = a/(b*c);
+	
+	return result;
+	}	
+
 int main(int argc, char *argv[]) {
-	int a =2;
-	a = square(a);
-	printf("a=%i\n",a);
- 
+	int n, r;
+	
+	printf("Input any number for n:");
+	scanf("%d", &n);
+	printf("Input any number for r:");
+	scanf("%d", &r);
+	
+	printf("c(%d, %d) = %d\n", n, r, com(n,r));
 	
 	return 0;
 }
