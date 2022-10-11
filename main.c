c1d9b402c7f34a9d7dce26decb9b1017304c93c3
soooyaya <hsoo0531@naver.com>
2022-10-11 17:05:22 +0900

    04

--- main.c
+++ main.c
@@ -3,35 +3,15 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
  
-int sumTwo(int a, int b)
-{
-	int sum = a+b;
-	return(sum);
-}
-
-int square(int n)
-{
-	return (n*n);
-}
-
-int get_max(int x, int y)
-{
-	if (x > y)
-	   return x;
-	else
-	   return y;
-}
- 
+ int square(int a)
+ {
+ 	return(a*a);
+ }
 int main(int argc, char *argv[]) {
-	int res_sum, res_square, res_get_max;
-	
-	res_sum = sumTwo(2,3);
-	res_square = square(5);
-	res_get_max = get_max(10,-20);
-	
-	printf("%i %i %i\n", res_sum, res_square, res_get_max);
-
-	
+	int a =2;
+	a = square(a);
+	printf("a=%i\n",a);
+ 
 	
 	return 0;
 }
